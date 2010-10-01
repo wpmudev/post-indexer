01b34bdbe6a2ff51c30887d09c3367ba945bb784
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-10-01 22:18:16 +0000

    update version 2.0
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/post-indexer@709 f53fe46e-8c02-459b-9fdd-93e75771b278

--- post-indexer.php
+++ post-indexer.php
@@ -1,15 +1,15 @@
 <?php
 /*
 Plugin Name: Post Indexer
-Plugin URI: 
+Plugin URI:
 Description:
 Author: Andrew Billits (Incsub)
-Version: 1.0.8
+Version: 2.0
 Author URI:
 WDP ID: 30
 */
 
-/* 
+/*
 Copyright 2007-2009 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
@@ -26,7 +26,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-$post_indexer_current_version = '1.0.8';
+$post_indexer_current_version = '2.0';
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
 //------------------------------------------------------------------------//
@@ -64,7 +64,7 @@ function post_indexer_make_current() {
 	if (get_site_option( "post_indexer_version" ) == '') {
 		add_site_option( 'post_indexer_version', '0.0.0' );
 	}
-	
+
 	if (get_site_option( "post_indexer_version" ) == $post_indexer_current_version) {
 		// do nothing
 	} else {
@@ -77,7 +77,7 @@ function post_indexer_make_current() {
 	if (get_option( "post_indexer_version" ) == '') {
 		add_option( 'post_indexer_version', '0.0.0' );
 	}
-	
+
 	if (get_option( "post_indexer_version" ) == $post_indexer_current_version) {
 		// do nothing
 	} else {
@@ -98,11 +98,11 @@ function post_indexer_global_install() {
 	if (get_site_option( "post_indexer_installed" ) == '') {
 		add_site_option( 'post_indexer_installed', 'no' );
 	}
-	
+
 	if (get_site_option( "post_indexer_installed" ) == "yes") {
 		// do nothing
 	} else {
-	
+
 		$post_indexer_table1 = "CREATE TABLE IF NOT EXISTS `" . $wpdb->base_prefix . "site_posts` (
   `site_post_id` bigint(20) unsigned NOT NULL auto_increment,
   `blog_id` bigint(20),
@@ -130,14 +130,30 @@ function post_indexer_global_install() {
   `term_count` bigint(20),
   PRIMARY KEY  (`term_count_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;";
-		$post_indexer_table3 = "";
-		$post_indexer_table4 = "";
+
+		$post_indexer_table3 = "CREATE TABLE IF NOT EXISTS `{$wpdb->base_prefix}site_terms` (
+	                `term_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	                `name` varchar(200) NOT NULL DEFAULT '',
+	                `slug` varchar(200) NOT NULL DEFAULT '',
+	                `type` varchar(20) NOT NULL DEFAULT 'product_category',
+	                `count` bigint(10) NOT NULL DEFAULT '0',
+	                PRIMARY KEY (`term_id`),
+	                UNIQUE KEY `slug` (`slug`),
+	                KEY `name` (`name`)
+	              ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+
+		$post_indexer_table4 = 	"CREATE TABLE IF NOT EXISTS `{$wpdb->base_prefix}site_term_relationships` (
+		                `site_post_id` bigint(20) unsigned NOT NULL,
+		                `term_id` bigint(20) unsigned NOT NULL,
+		                KEY (`site_post_id`),
+		                KEY (`term_id`)
+		              ) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
 		$post_indexer_table5 = "";
 
 		$wpdb->query( $post_indexer_table1 );
 		$wpdb->query( $post_indexer_table2 );
-		//$wpdb->query( $post_indexer_table3 );
-		//$wpdb->query( $post_indexer_table4 );
+		$wpdb->query( $post_indexer_table3 );
+		$wpdb->query( $post_indexer_table4 );
 		//$wpdb->query( $post_indexer_table5 );
 		update_site_option( "post_indexer_installed", "yes" );
 	}
@@ -153,9 +169,9 @@ function post_indexer_get_sort_terms($tmp_blog_ID){
 		$post_indexer_blog_types = '||';
 	}
 	$post_indexer_class = get_blog_option($tmp_blog_ID,"blog_class");
-	
+
 	$tmp_sort_terms = array();
-	
+
 	$post_indexer_blog_types = explode("|", $post_indexer_blog_types);
 	foreach ( $post_indexer_blog_types as $post_indexer_blog_type ) {
 		if ( $post_indexer_blog_type != '' ) {
@@ -165,22 +181,22 @@ function post_indexer_get_sort_terms($tmp_blog_ID){
 	if ( $post_indexer_class != '' ) {
 		$tmp_sort_terms[] = 'class_' . $post_indexer_class;
 	}
-	
+
 	$tmp_sort_terms[] = 'blog_lang_' . strtolower( $post_indexer_blog_lang );
-	
+
 	return '|' . implode("|", $tmp_sort_terms) . '|all|';
 
 }
 
 function post_indexer_post_insert_update($tmp_post_ID){
 	global $wpdb, $current_site;
-	
+
 	$tmp_blog_public = get_blog_status( $wpdb->blogid, 'public');
 	$tmp_blog_archived = get_blog_status( $wpdb->blogid, 'archived');
 	$tmp_blog_mature = get_blog_status( $wpdb->blogid, 'mature');
 	$tmp_blog_spam = get_blog_status( $wpdb->blogid, 'spam');
 	$tmp_blog_deleted = get_blog_status( $wpdb->blogid, 'deleted');
-	
+
 	$tmp_post = get_post($tmp_post_ID);
 
 	if ($tmp_post->post_type == 'page'){
@@ -219,8 +235,67 @@ function post_indexer_post_insert_update($tmp_post_ID){
 		(post_id, blog_id, site_id, sort_terms, post_author, post_title, post_content, post_content_stripped, post_permalink, post_published_gmt, post_published_stamp, post_modified_gmt, post_modified_stamp, post_terms, blog_public)
 		VALUES
 		('" . $tmp_post_ID . "','" . $wpdb->blogid . "','" . $wpdb->siteid . "','" . $tmp_sort_terms . "','" . $tmp_post->post_author . "','" . addslashes($tmp_post->post_title) . "','" . addslashes($tmp_post->post_content) . "','" . addslashes(post_indexer_strip_content($tmp_post->post_content)) . "','" . get_permalink($tmp_post_ID) . "','" . $tmp_post->post_date_gmt . "','" . strtotime($tmp_post->post_date_gmt) . "','" . $tmp_post->post_modified_gmt . "','" . time() . "','" . $tmp_post_terms . "','" . $tmp_blog_public . "')");
+
+		$site_post_id = $wpdb->insert_id;
+
 		//update term counts
 		post_indexer_post_terms_insert_update($tmp_sort_terms,$tmp_post_terms);
+
+		//get post terms
+		$taxonomies = array( 'post_tag', 'category' );
+		$new_terms = wp_get_object_terms( array( $tmp_post_ID ), $taxonomies );
+		if ( count($new_terms) ) {
+
+      		//get existing terms
+      		foreach ($new_terms as $term)
+		        $new_slugs[] = $term->slug;
+		  		$slug_list = implode( "','", $new_slugs );
+		      $existing_terms = $wpdb->get_results( "SELECT * FROM {$wpdb->base_prefix}site_terms WHERE slug IN ('$slug_list')" );
+		      $existing_slugs = array();
+		      if ( is_array($existing_terms) && count($existing_terms) ) {
+		        foreach ($existing_terms as $term) {
+		          $existing_slugs[$term->term_id] = $term->slug;
+		        }
+		      }
+
+        		//process
+				$tids = array();
+		        foreach ($new_terms as $term) {
+
+		          //check if in terms, but not attached
+		          if ( in_array($term->slug, $existing_slugs) ) {
+
+		            //add relationship
+		            $id = array_search($term->slug, $existing_slugs);
+		            $wpdb->insert( $wpdb->base_prefix . 'site_term_relationships', array( 'term_id' => $id, 'site_post_id' => $site_post_id ) );
+
+
+
+		          } else { //brand new term
+
+		            //insert term
+		            $wpdb->insert( $wpdb->base_prefix . 'site_terms', array( 'name' => $term->name, 'slug' => $term->slug, 'type' => $term->taxonomy ) );
+		            $id = $wpdb->insert_id;
+
+		            //add relationship
+		            $wpdb->insert( $wpdb->base_prefix . 'site_term_relationships', array( 'term_id' => $id, 'site_post_id' => $site_post_id ) );
+
+		          }
+
+					$tids[] = $id;
+
+		        }
+
+				// Update the post terms information
+				if(!empty($tids)) {
+					$wpdb->query( $wpdb->prepare("UPDATE " . $wpdb->base_prefix . "site_posts SET post_terms = %s WHERE site_post_id = %d", '|' . implode('|', $tids) .  '|', $site_post_id ));
+				}
+
+    	} else { //no terms, so adjust counts of existing
+
+      		//delete term relationships
+      		$wpdb->query( "DELETE FROM {$wpdb->base_prefix}site_term_relationships WHERE site_post_id = $site_post_id" );
+    	}
 	}
 }
 
@@ -231,7 +306,7 @@ function post_indexer_post_terms_insert_update($tmp_sort_terms,$tmp_post_terms){
 	$tmp_post_terms = explode("|",$tmp_post_terms);
 	$tmp_post_terms = array_unique($tmp_post_terms);
 	$tmp_sort_terms = explode("|",$tmp_sort_terms);
-	
+
 	$tmp_term_array_items = array();
 	$tmp_loop_count = 0;
 	foreach ( $tmp_sort_terms as $tmp_sort_term ) {
@@ -243,7 +318,7 @@ function post_indexer_post_terms_insert_update($tmp_sort_terms,$tmp_post_terms){
 			}
 		}
 	}
-	
+
     $existing_where = "WHERE ( ";
     $x = 1;
     foreach ($tmp_term_array_items as $tmp_term_array_item) {
@@ -254,10 +329,10 @@ function post_indexer_post_terms_insert_update($tmp_sort_terms,$tmp_post_terms){
            $existing_where .= " OR (term_id = '" . $tmp_term_array_item['id'] ."' AND term_count_type = '" . $tmp_term_array_item['type'] ."')";
        }
     }
-	
+
 	$existing_query = "SELECT * FROM " . $wpdb->base_prefix . "term_counts " . $existing_where . " )";
 	$tmp_existing_terms = $wpdb->get_results( $existing_query, ARRAY_A );
-	
+
 	foreach ( $tmp_sort_terms as $tmp_sort_term ) {
 		foreach ( $tmp_post_terms as $tmp_post_term ) {
 			if ( $tmp_sort_term != '' && $tmp_post_term != '' ) {
@@ -303,7 +378,7 @@ function post_indexer_delete($tmp_post_ID){
 	//adjust term counts
 	$tmp_post_terms = explode("|",$tmp_post_terms);
 	$tmp_sort_terms = explode("|",$tmp_sort_terms);
-	
+
 	$tmp_term_array_items = array();
 	$tmp_loop_count = 0;
 	foreach ( $tmp_sort_terms as $tmp_sort_term ) {
@@ -330,8 +405,18 @@ function post_indexer_delete($tmp_post_ID){
 	    $update_query = "UPDATE " . $wpdb->base_prefix . "term_counts SET term_count = term_count - 1,term_count_updated = '" . date("Y-m-d H:i:s") . "' ". $update_where . " )";
 	    $wpdb->query($update_query);
 	}
-	//delete site post
-	$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "site_posts WHERE post_id = '" . $tmp_post_ID . "' AND blog_id = '" . $wpdb->blogid . "'" );
+	// get the site_post_id
+	$site_post_id = $wpdb->get_var( "SELECT site_post_id FROM " . $wpdb->base_prefix . "site_posts WHERE post_id = '" . $tmp_post_ID . "' AND blog_id = '" . $wpdb->blogid . "'");
+
+	if(!empty($site_post_id)) {
+		//delete site post
+		$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "site_posts WHERE site_post_id = '" . $site_post_id . "'" );
+
+		// delete from site terms table
+		$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "site_term_relationships WHERE site_post_id = $site_post_id" );
+		//$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "site_terms WHERE term_id NOT IN (SELECT term_id FROM " . $wpdb->base_prefix . "site_term_relationships)" );
+	}
+
 }
 
 function post_indexer_delete_by_site_post_id($tmp_site_post_ID, $tmp_blog_ID) {
@@ -343,7 +428,7 @@ function post_indexer_delete_by_site_post_id($tmp_site_post_ID, $tmp_blog_ID) {
 	//adjust term counts
 	$tmp_post_terms = explode("|",$tmp_post_terms);
 	$tmp_sort_terms = explode("|",$tmp_sort_terms);
-	
+
 	$tmp_term_array_items = array();
 	$tmp_loop_count = 0;
 	foreach ( $tmp_sort_terms as $tmp_sort_term ) {
@@ -366,12 +451,16 @@ function post_indexer_delete_by_site_post_id($tmp_site_post_ID, $tmp_blog_ID) {
 			   $update_where .= " OR (term_id = '" . $tmp_term_array_item['id'] ."' AND term_count_type = '" . $tmp_term_array_item['type'] ."')";
 		   }
 		}
-	
+
 		$update_query = "UPDATE " . $wpdb->base_prefix . "term_counts SET term_count = term_count - 1,term_count_updated = '" . date("Y-m-d H:i:s") . "' ". $update_where . " )";
 		$wpdb->query($update_query);
 	}
 	//delete site post
 	$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "site_posts WHERE site_post_id = '" . $tmp_site_post_ID . "'" );
+
+	// delete from site terms table
+	$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "site_term_relationships WHERE site_post_id = $site_post_id" );
+	$wpdb->query( "DELETE FROM " . $wpdb->base_prefix . "site_terms WHERE term_id NOT IN (SELECT term_id FROM " . $wpdb->base_prefix . "site_term_relationships)" );
 }
 
 function post_indexer_public_update(){
