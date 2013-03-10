de88314a2f2843fabff63f90b39f6592fc873db4
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-03-10 15:37:55 +0000

    updated version number
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/post-indexer@8723 f53fe46e-8c02-459b-9fdd-93e75771b278

--- post-indexer.php
+++ post-indexer.php
@@ -4,14 +4,14 @@ Plugin Name: Post Indexer
 Plugin URI: http://premium.wpmudev.org/project/post-indexer/
 Description: Indexes all posts across your network and brings them into one spot – a very powerful tool that you use as a base to display posts in different ways or to manage your network.
 Author: Barry (Incsub)
-Version: 3.0.1
+Version: 3.0.2
 Author URI: http://premium.wpmudev.org
 WDP ID: 30
 Network: true
 */
 
 /*
-Copyright 2013 Incsub (http://incsub.com)
+Copyright Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
